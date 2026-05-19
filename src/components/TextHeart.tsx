89c01184636bf275b4631901394b3b8053e01072
Ramonsantosss <103340294+Ramonsantossss@users.noreply.github.com>
2026-05-19 14:23:25 -0300

    Update TextHeart.tsx

--- src/components/TextHeart.tsx
+++ src/components/TextHeart.tsx
@@ -19,7 +19,7 @@ export default function TextHeart() {
 
     let animationFrameId: number;
     let points: Point[] = [];
-    const text = "i love you";
+    const text = "Eu Amo Você";
     const fontSize = 14;
 
     const resize = () => {
